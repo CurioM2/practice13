5db50e1213e1813783a455ffee30c3190aaaa86b
Maxym Kuznetsov <madmen.kuznetsov.2009@gmail.com>
2025-07-06 20:59:09 +0200

    ну я там протестував там щось ну от ну от ну ви зрозуміли так ну добре ок ну там знаете я протестував там як один об'єкт ініціалізується і так далі ну коротш ви зрозуміли так

--- practice13/practice13.cpp
+++ practice13/practice13.cpp
@@ -8,6 +8,10 @@ int main() {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
 
+    //Book book;
+    //book.SetAuthor("template").SetName("sample").SetPublish("example").SetYear(0000).SetPageCount(000);
+    //book.PrintBook();
+
     const int size = 3;
     Book books[size] = {
         Book("Кузнецов Максим", "Що Робити Коли Ти Закохався У Вигаданого Персонажа І Тебе За Це Ненавидять", "Devolver Publishing House", 2025, 228),
@@ -18,5 +22,7 @@ int main() {
     PrintBooksByPublish(books, size);
     PrintBooksByYear(books, size);
 
+
 }
 
+ 
